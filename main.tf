0cafb3c101e0d9df4653698e5be73b16acbc768d
void269 <void269.269@gmail.com>
2026-09-24 13:29:18 -0400

    before changing direction

--- main.tf
+++ main.tf
@@ -1,13 +1,26 @@
 resource "vsphere_virtual_machine" "k8scontrolplane-prod01" {
   name = "k8scontrolplane-prod01"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore_cluster.datastore_cluster.id
+  datastore_cluster_id     = data.vsphere_datastore_cluster.datastore_cluster.id
   guest_id = data.vsphere_virtual_machine.template.guest_id
   network_interface {
     network_id = data.vsphere_network.network.id
   }
   clone {
     template_uuid = data.vsphere_virtual_machine.template.uuid
+    customize {
+      linux_options {
+        host_name = "k8scontrolplane-prod01"
+        domain    = var.domain_name
+      }
+      network_interface {
+        ipv4_address = var.k8scontrolplane_ip[0]
+        ipv4_netmask = var.netmask
+      }
+      ipv4_gateway = var.gateway_ip
+      dns_server_list = var.dns_servers
+      dns_suffix_list = var.dns_suffixes
+    }
   }
   disk {
     label            = "disk0"
@@ -20,7 +33,7 @@ resource "vsphere_virtual_machine" "k8scontrolplane-prod01" {
 resource "vsphere_virtual_machine" "k8sworker-prod01" {
   name = "k8sworker-prod01"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore_cluster.datastore_cluster.id
+  datastore_cluster_id     = data.vsphere_datastore_cluster.datastore_cluster.id
   guest_id = data.vsphere_virtual_machine.template.guest_id
   network_interface {
     network_id = data.vsphere_network.network.id
@@ -33,13 +46,16 @@ resource "vsphere_virtual_machine" "k8sworker-prod01" {
     size             = 20
     eagerly_scrub    = false
     thin_provisioned = true
+  }
+  cdrom {
+    client_device = true
   }
 }
 
 resource "vsphere_virtual_machine" "k8sworker-prod02" {
   name = "k8sworker-prod02"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore_cluster.datastore_cluster.id
+  datastore_cluster_id     = data.vsphere_datastore_cluster.datastore_cluster.id
   guest_id = data.vsphere_virtual_machine.template.guest_id
   network_interface {
     network_id = data.vsphere_network.network.id
@@ -52,5 +68,8 @@ resource "vsphere_virtual_machine" "k8sworker-prod02" {
     size             = 20
     eagerly_scrub    = false
     thin_provisioned = true
+  }
+  cdrom {
+    client_device = true
   }
 }
--- variables.tf
+++ variables.tf
@@ -43,5 +43,53 @@ variable "vsphere_network" {
 variable "vm_template_name" {
   description = "The name of the vSphere VM template to use"
   type        = string
-  default     = "ubuntu-noble-24.04-cloudimg"
+  default     = "ubuntu-26-04-template"
+}
+
+variable "controlplane_ip" {
+  description = "The IP address of the control plane node"
+  type        = list(string)
+  default     = ["10.0.1.17"]
+}
+
+variable "domain_name" {
+  description = "The domain name to use for the cluster"
+  type        = string
+  default     = "voidnet.com"
+}
+
+variable "dns_suffixes" {
+  description = "The DNS suffixes to use for the cluster"
+  type        = list(string)
+  default     = [var.domain_name]
+}
+
+variable "dns_servers" {
+  description = "The DNS servers to use for the cluster"
+  type        = list(string)
+  default     = ["10.0.1.2", "10.0.1.100", "8.8.8.8"]
+}
+
+variable "netmask" {
+  description = "The netmask to use for the cluster"
+  type        = string
+  default     = "24"
+}
+
+variable "gateway_ip" {
+  description = "The gateway IP address to use for the cluster"
+  type        = string
+  default     = "10.0.1.1"
+}
+
+variable "k8sworker_ip" {
+  description = "The IP addresses of the worker nodes"
+  type        = list(string)
+  default     = ["10.0.1.18", "10.0.1.19"]
+}
+
+variable "k8scontrolplane_ip" {
+  description = "The IP addresses of the control plane nodes"
+  type        = list(string)
+  default     = ["10.0.1.17"]
 }
